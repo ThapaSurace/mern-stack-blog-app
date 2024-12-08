89b18fb0012db8ddafd4986d48af7dfcac4b54d8
Suresh Thapa <zorosenpai456@gmail.com>
2024-12-08 15:23:17 +0545

    created component to show multiple post card

---
+++ src/components/PostList.jsx
@@ -0,0 +1,23 @@
+import PostCard from "./PostCard"
+
+const PostList = () => {
+    return (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-6">
+            <PostCard />
+            <PostCard />
+            <PostCard />
+            <PostCard />
+            <PostCard />
+            <PostCard />
+            <PostCard />
+            <PostCard />
+            <PostCard />
+            <PostCard />
+            <PostCard />
+            <PostCard />
+            <PostCard />
+            <PostCard />
+        </div>
+    )
+}
+export default PostList
