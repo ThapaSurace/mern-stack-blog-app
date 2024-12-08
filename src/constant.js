eeccfb84d879d98e8600e0ce8eb414125683aa12
Suresh Thapa <zorosenpai456@gmail.com>
2024-12-08 15:21:41 +0545

    constants for categories and navbar links added

---
+++ src/constant.js
@@ -0,0 +1,62 @@
+export const navLinks = [
+    {
+        id: 1,
+        name: "Home",
+        path: "/home"
+    },
+    {
+        id: 2,
+        name: "Trending",
+        path: "/trending"
+    },
+    {
+        id: 3,
+        name: "Contact",
+        path: "/contact"
+    },
+    {
+        id: 5,
+        name: "About",
+        path: "/about"
+    },
+]
+
+
+
+export const mainCat = [
+    {
+        id: 1,
+        name: "All Posts",
+        path: "/posts"
+    },
+    {
+        id: 2,
+        name: "Web Design",
+        path: "/posts?cat=web-design"
+    },
+    {
+        id: 3,
+        name: "Databases",
+        path: "/posts?cat=databases"
+    },
+    {
+        id: 5,
+        name: "Development",
+        path: "/posts?cat=development"
+    },
+    {
+        id: 5,
+        name: "Search Engines",
+        path: "/posts?cat=seo"
+    },
+    {
+        id: 6,
+        name: "Marketing",
+        path: "/posts?cat=marketing"
+    },
+    {
+        id: 7,
+        name: "AI",
+        path: "/posts?cat=ai"
+    },
+]
