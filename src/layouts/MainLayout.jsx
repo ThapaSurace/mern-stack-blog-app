a3e030b5e86e56d02a480ed01038a6c837ad4e07
Suresh Thapa <zorosenpai456@gmail.com>
2024-12-08 15:26:25 +0545

    refactor css

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,9 +1,9 @@
 import { Outlet } from "react-router-dom"
-import NavBar from "../components/NavBar"
+import NavBar from "../components/navbar/NavBar"
 
 const MainLayout = () => {
     return (
-        <div className="px-4 md:px-8 lg:px-16 lx:px-32 2xl:px-64">
+        <div className="px-6 md:px-8 lg:px-16">
             <NavBar />
             <Outlet />
         </div>
